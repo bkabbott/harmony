dba2be580b442e95a4df76f6381cd10ac11c448f
Brian Abbott <hi@brianabbott.com>
2026-02-16 18:25:35 -0500

    init commit

---
+++ harmony/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * File: ${NAME}.java
+ * Class: CSCI 1302
+ * Author: Brian Abbott
+ * Created on: 2/16/26
+ * Last modified: 2/16/26
+ * Description: To be filled in by the user.
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
