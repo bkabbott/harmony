63eb3ae1544e3854486f9702492178d5e228b8f7
Brian Abbott <hi@brianabbott.com>
2026-02-18 09:06:19 -0500

    add test code

--- harmony/src/Main.java
+++ harmony/src/Main.java
@@ -1,3 +1,6 @@
+import com.staffcasts.harmony.Triad;
+import com.staffcasts.harmony.TriadQuality;
+
 /**
  * File: ${NAME}.java
  * Class: CSCI 1302
@@ -9,14 +12,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Triad cMajor = new Triad();
+        Triad cMinor = new Triad("C", TriadQuality.MINOR);
+        System.out.println(cMajor.getChord());
+        System.out.println(cMinor.getChord());
     }
 }
